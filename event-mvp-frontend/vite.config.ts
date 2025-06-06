ea906a70bdc85fcb97c90e386ef890f788829c3d
Koderinjo <mebbon@protonmail.com>
2025-06-06 09:20:19 +0200

    .

--- event-mvp-frontend/vite.config.ts
+++ event-mvp-frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     base: "./",
-    plugins: [plugin()],
+    plugins: [react()],
     server: {
         port: 51417,
     },
     build: {
         outDir: 'build',
+        target: 'esnext',
+        emptyOutDir: true,
     },
 });
